65761775e1f5ae7e7bae784213d22104e5dafc5d
ellery russell <err258@nyu.edu>
2015-06-26 20:09:10 -0400

    fixed implementation of col2im and im2col gpu

--- src/caffe/util/im2col.cu
+++ src/caffe/util/im2col.cu
@@ -17,20 +17,20 @@ __global__ void im2col_gpu_kernel(const int n, const Dtype* data_im,
     const int kernel_stride_h, const int kernel_stride_w,
     const int kernel_h_reduced, const int kernel_w_reduced,
     Dtype* data_col) {
-  CUDA_KERNEL_LOOP(index, n) { // TODO implement subsample in gpu kernel
+  CUDA_KERNEL_LOOP(index, n) {
     int w_out = index % width_col;
     int h_index = index / width_col;
     int h_out = h_index % height_col;
     int channel_in = h_index / height_col;
-    int channel_out = channel_in * kernel_h * kernel_w;
+    int channel_out = channel_in * kernel_h_reduced * kernel_w_reduced;
     int h_in = h_out * stride_h - pad_h;
     int w_in = w_out * stride_w - pad_w;
     Dtype* data_col_ptr = data_col;
     data_col_ptr += (channel_out * height_col + h_out) * width_col + w_out;
     const Dtype* data_im_ptr = data_im;
     data_im_ptr += (channel_in * height + h_in) * width + w_in;
-    for (int i = 0; i < kernel_h; ++i) {
-      for (int j = 0; j < kernel_w; ++j) {
+    for (int i = 0; i < kernel_h; i += kernel_stride_h) {
+      for (int j = 0; j < kernel_w; j += kernel_stride_w) {
         int h = h_in + i;
         int w = w_in + j;
         *data_col_ptr = (h >= 0 && w >= 0 && h < height && w < width) ?
@@ -85,8 +85,8 @@ __global__ void col2im_gpu_kernel(const int n, const Dtype* data_col,
     const int pad_h, const int pad_w,
     const int stride_h, const int stride_w,
     const int height_col, const int width_col,
-    const int kernel_stride_h, const int kernel_stride_w,
-    const int kernel_h_reduced, const int kernel_w_reduced,
+    const int patch_stride_h, const int patch_stride_w,
+    const int patch_h_reduced, const int patch_w_reduced,
     Dtype* data_im) {
   CUDA_KERNEL_LOOP(index, n) {
     Dtype val = 0;  // TODO implement subsample in gpu kernel
@@ -94,10 +94,12 @@ __global__ void col2im_gpu_kernel(const int n, const Dtype* data_col,
     int h = (index / width) % height + pad_h;
     int c = index / (width * height);
     // compute the start and end of the output
-    int w_col_start = (w < patch_w) ? 0 : (w - patch_w) / stride_w + 1;
-    int w_col_end = min(w / stride_w + 1, width_col);
-    int h_col_start = (h < patch_h) ? 0 : (h - patch_h) / stride_h + 1;
-    int h_col_end = min(h / stride_h + 1, height_col);
+    int w_col_start = (w < patch_w) ? 0 : 
+      ((w - patch_w) / stride_w + 1) / patch_stride_w;
+    int w_col_end = min(w / stride_w + 1, width_col) / patch_stride_w;
+    int h_col_start = (h < patch_h) ? 0 : 
+      ((h - patch_h) / stride_h + 1) / patch_stride_h;
+    int h_col_end = min(h / stride_h + 1, height_col) / patch_stride_h;
     /*
     for (int h_col = h_col_start; h_col < h_col_end; ++h_col) {
       for (int w_col = w_col_start; w_col < w_col_end; ++w_col) {
@@ -110,8 +112,9 @@ __global__ void col2im_gpu_kernel(const int n, const Dtype* data_col,
     */
     // equivalent implementation
     int offset =
-        (c * patch_h * patch_w + h * patch_w + w) * height_col * width_col;
-    int coeff_h_col = (1 - stride_h * patch_w * height_col) * width_col;
+        (c * patch_h_reduced * patch_w_reduced + h * patch_w_reduced + w) * 
+        height_col * width_col;
+    int coeff_h_col = (1 - stride_h * patch_w_reduced * height_col) * width_col;
     int coeff_w_col = (1 - stride_w * height_col * width_col);
     for (int h_col = h_col_start; h_col < h_col_end; ++h_col) {
       for (int w_col = w_col_start; w_col < w_col_end; ++w_col) {
@@ -126,9 +129,9 @@ template <typename Dtype>
 void col2im_gpu(const Dtype* data_col, const int channels,
     const int height, const int width, const int patch_h, const int patch_w,
     const int pad_h, const int pad_w, const int stride_h,
-    const int stride_w, const int kernel_stride_h, 
-    const int kernel_stride_w, const int kernel_h_reduced, 
-    const int kernel_w_reduced, Dtype* data_im) {
+    const int stride_w, const int patch_stride_h, 
+    const int patch_stride_w, const int patch_h_reduced, 
+    const int patch_w_reduced, Dtype* data_im) {
   int height_col = (height + 2 * pad_h - patch_h) / stride_h + 1;
   int width_col = (width + 2 * pad_w - patch_w) / stride_w + 1;
   int num_kernels = channels * height * width;
@@ -139,8 +142,8 @@ void col2im_gpu(const Dtype* data_col, const int channels,
                              CAFFE_CUDA_NUM_THREADS>>>(
       num_kernels, data_col, height, width, channels, patch_h, patch_w,
       pad_h, pad_w, stride_h, stride_w,
-      height_col, width_col, kernel_stride_h,
-      kernel_stride_w, kernel_h_reduced, kernel_w_reduced, data_im);
+      height_col, width_col, patch_stride_h,
+      patch_stride_w, patch_h_reduced, patch_w_reduced, data_im);
   CUDA_POST_KERNEL_CHECK;
 }
 
