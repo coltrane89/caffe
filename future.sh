5dac073708c5815b2f3801b99b49ffa0266459c4
coltrane89 <err258@nyu.edu>
2015-07-28 20:50:48 -0400

    fix bad merge from cropLayer

--- future.sh
+++ future.sh
@@ -3,18 +3,17 @@ git checkout master
 git branch -D future
 git checkout -b future
 ## merge PRs
-# coord maps, net pointer, crop layer
+# coord maps, net pointer, crop layer # THIS NEEDS TO BE FIXEDDDDDD
 hub merge https://github.com/BVLC/caffe/pull/1976
-# gradient accumulation
-hub merge https://github.com/BVLC/caffe/pull/1977
-# python net spec
-hub merge https://github.com/BVLC/caffe/pull/2086
+# shared im2col for reducing memory usage
+hub merge https://github.com/BVLC/caffe/pull/2009
 ## commit
+
 cat <<"END" > README.md
 This is a pre-release Caffe branch for fully convolutional networks. This includes unmerged PRs and no guarantees.
 Everything here is subject to change, including the history of this branch.
-Consider PR #2016 for reducing memory usage.
 See `future.sh` for details.
+
 END
 git add README.md
 git add future.sh
